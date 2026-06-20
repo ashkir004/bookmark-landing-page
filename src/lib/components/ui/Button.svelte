3268d9f9bf25dc3a841ae91e8d32faf987082136
ashkir004 <ashkir004@gmail.com>
2026-06-20 20:49:09 +0300

    setup + Button component

---
+++ src/lib/components/ui/Button.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { cn } from "$lib/utils";
+
+    type Variant = 'solid' | 'outline';
+
+    interface ButtonProps {
+        variant?: Variant;
+        class?: string;
+        disabled?: boolean;
+        text?: string;
+    }
+
+    let {
+        variant= 'solid',
+        class: className= '',
+        disabled= false,
+        text= 'Button'
+    }: ButtonProps = $props();
+
+    const variants = {
+        solid: { 
+            default: `bg-primary text-white shadow-button cursor-pointer`,
+            disabled: `bg-neutral-light-gray text-neutral-dark-gray shadow-button/50 cursor-not-allowed`
+         },
+        outline: { 
+            default: `border border-primary text-primary bg-white cursor-pointer`,
+            disabled: `border border-2 border-neutral-dark-gray text-neutral-dark-gray bg-white cursor-not-allowed`
+        } 
+    };
+
+</script>
+
+<button
+    disabled={disabled}
+    class={cn(`inline-flex items-center justify-center rounded-md px-8 py-3`,
+        variants[variant][disabled ? 'disabled' : 'default'],
+        className
+    )}
+>
+    {text}
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Button from "$lib/components/ui/Button.svelte";
+</script>
+
+<div class="inner-container flex flex-col items-center justify-center h-screen gap-6 text-center">
+    <Button variant="solid" text="Get it on Chrome" />
+    <Button variant="outline" text="Get it on Firefox" />
+
+    <Button variant="solid" text="Disabled Button" disabled={true} />
+    <Button variant="outline" text="Disabled Button" disabled={true} />
+</div>
