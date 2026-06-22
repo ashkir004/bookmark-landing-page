236ab8cd87a2475b4979d2eb4024576b5a77300b
ashkir004 <ashkir004@gmail.com>
2026-06-22 20:04:08 +0300

    finished hero layout

--- src/lib/components/layout/Hero.svelte
+++ src/lib/components/layout/Hero.svelte
@@ -3,17 +3,19 @@
     import HeroIllustration from '$lib/assets/images/illustration-hero.svg';
 </script>
 
-<div class="flex flex-col gap-12 py-8 justify-items-center">
-    <div class="grid grid-cols-1 grid-rows-1 justify-items-center items-start relative">
-        <span class="col-span-full row-span-full bg-primary inline-block w-[80%] h-[78%] md:w-[70%] self-end justify-self-end rounded-l-full"></span>
+<div class="flex flex-col gap-12 lg:gap-8 justify-items-end lg:grid lg:grid-cols-2 lg:items-center">
+    <div class="grid grid-cols-1 grid-rows-1 justify-items-center items-start">
+        <span aria-hidden="true" class="col-span-full row-span-full bg-primary inline-block w-[80%] h-[78%] md:w-[70%] self-end justify-self-end rounded-l-full"></span>
         <img class="col-span-full row-span-full" src={HeroIllustration} alt="Hero: A bookmark Manager Screen" />
     </div>
-    <div class="flex flex-col gap-6 text-center px-6 md:px-16">
-        <h1 class="text-display text-center text-secondary capitalize">A Simple Bookmark Manager</h1>
-        <p class="text-body md:text-lead text-muted/70">A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
-    </div>
-    <div class="flex flex-row gap-4 px-6 md:px-16 items-center justify-center ">
-        <Button variant="solid" text="Get it on Chrome" class="px-3 py-3" />
-        <Button variant="solidSecondary" text="Get it on Firefox" class="px-3 py-3" />
+    <div class="flex flex-col gap-6 col-start-1 row-start-1">
+        <div class="flex flex-col gap-6 text-center px-6 md:px-16 lg:pl-16 lg:pr-0 lg:text-start">
+            <h1 class="text-display text-center lg:text-start text-secondary capitalize">A Simple Bookmark Manager</h1>
+            <p class="text-body md:text-lead text-muted/70 text-pretty">A clean and simple interface to organize your favourite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+        </div>
+        <div class="flex flex-row gap-4 px-6 md:px-16 items-center justify-center lg:justify-start">
+            <Button variant="solid" text="Get it on Chrome" class="px-3 py-3 md:px-6" />
+            <Button variant="solidSecondary" text="Get it on Firefox" class="px-3 py-3 md:px-6" />
+        </div>
     </div>
 </div>
